182dbc0dad563cb5e5a96c2afac6569fbab62240
wetzernutz <126750514+wetzernutz@users.noreply.github.com>
2024-06-07 17:07:28 +1000

    Added javadoc to Parser and ParserTest

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -2,14 +2,106 @@ package parser;
 
 import java.io.*;
 
+/**
+ * A simple predictive parser to convert infix notation to postfix notation.
+ * <p>
+ * This parser uses the top-down method of syntax analysis called
+ *  <a href="https://en.wikipedia.org/wiki/Top-down_parsing">top-down parsing</a>, where construction starts
+ * from the root down to the leafs.     The parsing method chosen is the recursive descent parsing
+ * method which uses a set of recursive procedures to process input.
+ * <blockquote><pre>
+ * Translation scheme:
+ *
+ * expr -> term rest
+ *
+ * rest -> + term { print('+') } rest
+ *      |  - term { print('-') } rest
+ *      |  ''
+ *
+ * term -> 0 { print('0') }
+ *      |  1 { print('1') }
+ *      ...
+ *      |  9 { print('9') }
+ * </pre></blockquote>
+ *
+ * <p>
+ * This parser implements the simplest form of
+ * <a href="https://en.wikipedia.org/wiki/Recursive_descent_parser">recursive-descent</a>
+ * called predictive parsing,
+ * where the first symbol in the input is always sufficient in determining which production
+ * must be used. Hence, only a lookahead symbol that stores one char is used.
+ * </p>
+ *
+ * Note:  White space is not skipped by this parser when matching.
+ */
 class Parser {
     static int lookahead;
 
     public Parser() throws IOException {
-        this.lookahead = System.in.read();
+        lookahead = System.in.read();
     }
 
-    void expr() throws IOException {
+    /**
+     * Method implementing the expr productions.
+     * <blockquote><pre>
+     * expr -> term rest
+     * rest -> + term { print('+') } rest
+     *      |  - term { print('-') } rest
+     *      |  ''
+     * </pre></blockquote>
+     *
+     * <p>
+     * This implementation inlines the rest productions into expr,
+     * the steps taken to do this are:
+     * <ol>
+     * <li>Initial rest method
+     * <pre>{@code
+     * void rest() {
+     *     if (lookahead=='+') {
+     *         match('+'); term(); print('+'); ;
+     *     } else if (lookahead=='-') {
+     *         match('-'); term(); print('-'); continue;
+     *     } else {} // do nothing
+     * }
+     * }</pre>
+     * </li>
+     * <li>Eliminate Tail Recursion using iteration:
+     * <pre>{@code
+     * void rest() {
+     *     while(true) {
+     *         if (lookahead=='+') {
+     *             match('+'); term(); print('+'); continue;
+     *         } else if (lookahead=='-') {
+     *             match('-'); term(); print('-'); continue;
+     *         } else {}//do nothing
+     *         break;
+     *     }
+     * }
+     * }</pre>
+     * </li>
+     * <li>Inline rest into rest call in <b>expr -> term rest</b>:
+     * <pre>{@code
+     * void expr() throws IOException{
+     *     term();
+     *     // was previously a single rest() call below
+     *     while(true){
+     *         if(lookahead == '+') {
+     *             match('+'); term(); System.out.write('+'); continue;
+     *         } else if (lookahead == '-') {
+     *             match('-'); term(); System.out.write('-'); continue;
+     *         }
+     *         break;
+     *     }
+     * }
+     * }</pre>
+     * </li>
+     * </ol></p>
+     * <p>Note: In this implementation empty str is defaulted when no
+     * productions can be used for expr.
+     * </p>
+     * @throws IOException if an I/ O error occurs while matching
+     */
+    void expr() throws IOException{
         term();
         while(true){
             if(lookahead == '+') {
@@ -21,13 +113,32 @@ class Parser {
         }
     }
 
-    void term() throws IOException {
+    /**
+     * Method representing the term productions.
+     * <blockquote><pre>
+     * term -> 0 { print('0') }
+     *      |  1 { print('1') }
+     *      ...
+     *      |  9 { print('9') }
+     * </pre></blockquote>
+     * <p> Simply matches a digit lookahead.</p>
+     * @throws IOException – if an I/ O error occurs
+     * @throws Error syntax error when lookahead is not a digit
+     */
+    void term() throws IOException, Error{
         if ( Character.isDigit((char)lookahead) ) {
-            System.out.write((char) lookahead); match(lookahead);
+            System.out.write((char)lookahead); match(lookahead);
         } else throw new Error("syntax error");
     }
 
-    void match(int t) throws IOException {
+    /**
+     * Matches the current lookahead with a character and
+     * forwards the lookahead.  Note that matching doesn't skip white space.
+     * @param t character to match with lookahead
+     * @throws IOException – if an I/ O error occurs
+     * @throws Error Syntax error when the lookahead doesn't match with t
+     */
+    void match(int t) throws IOException, Error{
         if(lookahead == t) lookahead = System.in.read();
         else throw new Error("syntax error");
     }
---
+++ src/parser/ParserTest.java
@@ -0,0 +1,145 @@
+package parser;
+
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+
+/**
+ * This class implements unit tests for {@link Parser} using
+ * <a href="https://junit.org/junit5/docs/current/api/">JUnit</a>.
+ *
+ * <p>Notable attributes in this class that
+ * enable the unit tests to work are:</p>
+ * <ul>
+ * <li>
+ * A byte array output stream that serves as a temporary buffer to capture the output data
+ * produced by the Parser class during testing. This is used to manually set the input read by
+ * {@link java.io.InputStream#read() System.in.read()}. The stream is reset before each test
+ * to ensure it only contains data generated during the current test execution.
+ * <pre>{@code
+ *    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+ * }</pre>
+ * </li>
+ *
+ * <li>
+ * A reference to the original standard output stream {@link java.lang.System#out System.out}before it is redirected.
+ * Used to restore the standard output to its original state after the tests have
+ * been executed.
+ * <pre>{@code
+ *    PrintStream originalOut = System.out;
+ * }</pre>
+ * </li>
+ *
+ * <li>
+ * A reference to the original standard input stream {@link java.lang.System#in System.in}before it is redirected.
+ * Used to restore the standard output to its original state after the tests have
+ * been executed.
+ * <pre>{@code
+ *   InputStream originalIn = System.in;
+ * }</pre>
+ * </li>
+ * </ul>
+ */
+class ParserTest {
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private static final PrintStream originalOut = System.out;
+    private static final InputStream originalIn = System.in;
+
+    /**
+     * Reassigns the "standard" output stream into a new {@link java.io.PrintStream} using our {@link ByteArrayOutputStream} that
+     * we have created as the output data.
+     * <pre>{@code
+     *    System.setOut(new PrintStream(ByteArrayOutputStream outContent));
+     * }</pre>
+     * This is such that instead of fetching the result from the standard output stream and calling
+     * {@link java.io.PrintStream#println()} (System.out is the standard output stream as type PrintStream)
+     * as usual, it would use a Stream that has the data defined in our own custom {@link PrintStream}.
+     * We would later use {@link PrintStream#toString() toString} to fetch and verify the output.
+     * @see     java.io.PrintStream#toString()
+     * @see     java.io.PrintStream#println()
+     */
+    @BeforeAll
+    static void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    /**
+     * Reassigns back the Standard input and output streams to normal after testing has completed.
+     */
+    @AfterAll
+    static void restoreStreams() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
+    /**
+     * Resets the data stored within the byteArray to clear the previous results.
+     */
+    @BeforeEach
+    void setUp() {
+        outContent.reset();
+    }
+
+    /**
+     * Sets up the inputStream with a predefined input to mimic a user's input.
+     * Used to mimic the input fetched by {@link java.io.InputStream#read() System.in.read()}.
+     * @param input the input that is expected to be 'read' from the user.
+     */
+    private void setInputStream(String input) {
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testExpr() throws IOException {
+        setInputStream("1+2-4");
+        Parser parser = new Parser();
+        parser.expr();
+        Assertions.assertEquals("12+4-", outContent.toString().trim());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTerm() throws IOException {
+        String input = "7";
+        setInputStream(input);
+
+        Parser parser = new Parser();
+        parser.term();
+        Assertions.assertEquals(input, outContent.toString().trim());
+    }
+
+    @Test
+    void testTermInvalid() throws IOException {
+        String input = "a";
+        setInputStream(input);
+
+        Parser parser = new Parser();
+        Assertions.assertThrows(Error.class, parser::term);
+    }
+
+
+    @Test
+    void testMatchSimple() throws IOException {
+        String input = "78";
+        setInputStream(input);
+
+        Parser parser = new Parser();
+        parser.match('7');
+        parser.match('8');
+        Assertions.assertEquals(-1, Parser.lookahead);
+    }
+
+    @Test
+    void testMatchWhiteSpace() throws IOException {
+        String input = " 7 8";
+        setInputStream(input);
+
+        Parser parser = new Parser();
+        Assertions.assertThrows(Error.class, () -> parser.match('7'));
+        for (char c : input.toCharArray()) {
+            parser.match(c);
+        }
+        Assertions.assertEquals(-1, Parser.lookahead);
+    }
+
+}
